5cb44bf25fa07ac3e4f01eb42d2ebcb99c940813
Kakul Sahu <kakulsahu1511@outlook.com>
2022-05-28 23:25:46 +0530

    Changes

--- src/Login.js
+++ src/Login.js
@@ -2,15 +2,15 @@ import React from 'react'
 import axios from 'axios';
 
 export default function Login() {
-    const hello = ()=>{
+    const hello = () => {
         axios.post('https://kakulengagefaceauthenticatorv1.azurewebsites.net//api/auth/signin', 'hello')
     }
     return (
-    <>
-        <div>
-            <button onClick={hello}></button>
-        </div>
-  
-    </>
-  )
+        <>
+            <div>
+                <button onClick={hello}>Hello kakul</button>
+            </div>
+
+        </>
+    )
 }
